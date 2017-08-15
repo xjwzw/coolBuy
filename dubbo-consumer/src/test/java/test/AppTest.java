dc9117cd265228beed70be936c078b4aeab403ea
xjwzw <xjwangzhenwei243@163.com>
2017-08-15 23:04:04 +0800

    2017/8/15

--- dubbo-consumer/src/test/java/test/AppTest.java
+++ dubbo-consumer/src/test/java/test/AppTest.java
@@ -6,6 +6,6 @@ public class AppTest {
 	public static void main(String[] args) {
 		ConsumerThd thd = new ConsumerThd();
 		thd.sayHello();
-		System.out.println("hello world")
+		System.out.println("hello world");
 	}
 }
